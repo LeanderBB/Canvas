278faa4a6db363ce840ccf563dfa8c1982edc594
Leander Bessa Beernaert <leanderbb@gmail.com>
2012-07-05 18:44:13 +0100

    Added jslint tool available in tools/jslint, updated js files to pass the jslint tool. Added video player to core, see documentation for more details

--- apps/gallery/app.js
+++ apps/gallery/app.js
@@ -1,215 +1,215 @@
 
-window.onload = function(){
+window.onload = function () {
     g = new Gallery();
     g.init();
-}
+};
 
-function Gallery(){
+function Gallery() {
     this.menu_view = new Gallery.MenuView(this);
     this.model = new Gallery.Model();
     this.content_view = new Gallery.ContentView(this);
     var that = this;
-
-
 }
 
-Gallery.prototype.init = function(){
+Gallery.prototype.init = function () {
     this.model.init();
-    this.menu_view.load(this.model); 
-    this.menu_view.openGallery(0); 
-}
+    this.menu_view.load(this.model);
+    this.menu_view.openGallery(0);
+};
 
-Gallery.prototype.openGallery = function (index){
+Gallery.prototype.openGallery = function (index) {
     var gallery = this.model.getGalleryAtIndex(index);
     this.content_view.displayGallery(gallery);
-}
+};
 
 Gallery.DataModels = {};
 Gallery.ContentHandlers = {};
-Gallery.MediaDir ="../../media/";
+Gallery.MediaDir = "../../media/";
 // ### Gallery Model #############################################
 
-Gallery.Model = function (){
+Gallery.Model = function () {
     this.galleries = [];
-}
+};
 
-Gallery.Model.prototype.getGalleryCoubt = function(){
+Gallery.Model.prototype.getGalleryCoubt = function () {
     return this.galleries.length;
-}
+};
 
-Gallery.Model.prototype.getGalleries = function(){
+Gallery.Model.prototype.getGalleries = function () {
     return this.galleries;
-}
+};
 
-Gallery.Model.prototype.getGalleryAtIndex = function(index){
-    return this.galleries[index]
-}
+Gallery.Model.prototype.getGalleryAtIndex = function (index) {
+    return this.galleries[index];
+};
 
-Gallery.Model.prototype.init = function() {
-    var item;
-    for ( i in CONFIG.gallery){
+Gallery.Model.prototype.init = function () {
+    var item, i, model;
+    for (i in CONFIG.gallery) {
         item = CONFIG.gallery[i];
-        if ( Gallery.DataModels[item.type] === "undefined" || 
-                typeof Gallery.DataModels[item.type] !== "function"){
-            alert("data-model-not-found: "+item.type);
-        }else{
+        if (Gallery.DataModels[item.type] === "undefined" ||
+                typeof Gallery.DataModels[item.type] !== "function") {
+            alert("data-model-not-found: " + item.type);
+        } else {
             model = new Gallery.DataModels[item.type]();
             model.init(item);
             this.galleries.push(model);
         }
     }
-}
+};
 
 
 // ### Gallery Standard Data Model ###############################
 
-function StandardModel(){
+function StandardModel() {
     this.name = "";
     this.type = "standard";
     this.files = [];
     this.thumb_url = "";
 }
 
-StandardModel.prototype.getName = function() {
+StandardModel.prototype.getName = function () {
     return this.name;
-}
+};
 
-StandardModel.prototype.getType = function() {
+StandardModel.prototype.getType = function () {
     return this.type;
-}
+};
 
-StandardModel.prototype.getThumbUrl = function() {
+StandardModel.prototype.getThumbUrl = function () {
     return Gallery.MediaDir + this.thumb_url;
-}
+};
 
-StandardModel.prototype.getItemCount = function() {
+StandardModel.prototype.getItemCount = function () {
     return this.files.length;
-}
+};
 
-StandardModel.prototype.getItem = function( index ){
-    return this.files[index]; 
-}
+StandardModel.prototype.getItem = function (index) {
+    return this.files[index];
+};
 
-StandardModel.prototype.newElement = function(){
+StandardModel.prototype.newElement = function () {
     return new StandardModel.Element();
-}
+};
 
-StandardModel.prototype.init = function(data){
+StandardModel.prototype.init = function (data) {
     this.name = data.name;
     this.thumb_url = data.thumb;
-    var i;
-    for ( i in data.data){
-        var element = this.newElement();
+    var i, element;
+    for (i in data.data) {
+        element = this.newElement();
         element.init(data.data[i]);
         this.files.push(element);
     }
-}
+};
 
-StandardModel.Element = function(){
+StandardModel.Element = function () {
     this.url = "";
     this.thumb_url = "";
     this.item_type = "";
-}
+};
 
 StandardModel.Element.prototype.getUrl = function () {
     return  Gallery.MediaDir + this.url;
-}
+};
 StandardModel.Element.prototype.getThumbUrl = function () {
     return  Gallery.MediaDir + this.thumb_url;
-}
+};
 
 StandardModel.Element.prototype.getItemType = function () {
     return this.item_type;
-}
+};
 
 StandardModel.Element.prototype.getType = function () {
     return "standard.element";
-}
+};
 
-StandardModel.Element.prototype.init = function (data) { 
+StandardModel.Element.prototype.init = function (data) {
     this.url = data.url;
     this.item_type = data.type;
     this.thumb_url = data.thumb;
-}
+};
 
 Gallery.DataModels["standard"] = StandardModel;
 // ### Gallery Menu View #########################################
 
-Gallery.MenuView = function(gallery){
+Gallery.MenuView = function (gallery) {
     this.controller = gallery;
-    this.menu= new Slider({div:"menu",mode:Slider.SCROLL_VERTICAL});
+    this.menu = new Slider({div: "menu", mode: Slider.SCROLL_VERTICAL});
     this.list = document.createElement("ul");
     this.list.classList.add("g_menu");
-    this.element_count=0;
+    this.element_count = 0;
     this.icon_top = document.getElementById("menu_top");
     this.icon_bottom = document.getElementById("menu_bottom");
     this.current_element = null;
     this.elements = [];
-}
+};
 
-Gallery.MenuView.prototype.handleScroll = function(scroll){
-    if( scroll.y > 50 ){
+Gallery.MenuView.prototype.handleScroll = function (scroll) {
+    if (scroll.y > 50) {
         this.icon_top.style.visibility = "visible";
-    }else{
+    } else {
         this.icon_top.style.visibility = "hidden";
     }
 
-    if ( scroll.y >= (this.menu._getLimit().y * -1 - 50)){
+    if (scroll.y >= (this.menu._getLimit().y * -1 - 50)) {
         this.icon_bottom.style.visibility = "hidden";
-    }else{
+    } else {
         this.icon_bottom.style.visibility = "visible";
     }
-}
+};
 
-Gallery.MenuView.prototype.load = function(model){
-    var galleries = model.getGalleries();
+Gallery.MenuView.prototype.load = function (model) {
+    var galleries = model.getGalleries(), i;
     this.menu.clear();
     this.menu.getView().appendChild(this.list);
     this.element_count = 0;
-    var i;
     this.icon_top.style.visibility = "hidden";
     this.icon_bottom.style.visibility = "hidden";
-    for ( i in galleries ){
+    for (i in galleries ) {
         this.addElement(galleries[i]);
     }
-    if(this.menu.isSlidable()){
+    if (this.menu.isSlidable()) {
         this.icon_bottom.style.visibility = "visible";
-        this.menu.EventScroll.addListener(this,this.handleScroll);
+        this.menu.EventScroll.addListener(this, this.handleScroll);
     }
-}
+};
 
-Gallery.MenuView.prototype.addElement = function(element){
+Gallery.MenuView.prototype.addElement = function (element) {
 
-    var elem_view = document.createElement("li");
-    elem_view.id=this.element_count;
-    elem_view.innerHTML = '<div style="background-image: url(\''+
-        element.getThumbUrl()+'\')"></div><h1>'+element.getName()+
-        "</h1><h2>"+element.getItemCount()+" Items</h2>"
-        this.list.appendChild(elem_view);
-    var that = this;
-    var count = this.element_count;
+    var elem_view = document.createElement("li"),
+        that = this, count = this.element_count;
+    elem_view.id = this.element_count;
+    elem_view.innerHTML = '<div style="background-image: url(\'' +
+        element.getThumbUrl() + '\')"></div><h1>' + element.getName() +
+        "</h1><h2>" + element.getItemCount() + " Items</h2>";
+    this.list.appendChild(elem_view);
     this.elements.push(elem_view);
-    elem_view.addEventListener("mouseup",function(){
+    elem_view.addEventListener("mouseup", function () {
             that.openGallery(count);
-            },false);
+        }, false);
     this.element_count++;
-}
+};
+
 Gallery.MenuView.prototype.selectElement = function (index) {
-    if (this.current_element != null){
+    if (this.current_element !== null) {
         this.current_element.classList.remove("g_menu_active");
     }
     this.current_element = this.elements[index];
     this.current_element.classList.add("g_menu_active");
-}
-Gallery.MenuView.prototype.openGallery = function(gallery_index){
+};
+
+Gallery.MenuView.prototype.openGallery = function (gallery_index) {
     this.controller.openGallery(gallery_index);
     this.selectElement(gallery_index);
-}
+};
 
 // ### Gallery Content View #######################################
 
-Gallery.ContentView = function(gallery){
-    this.content = new Slider({div:"content", mode:Slider.SCROLL_HORIZONTAL,disable_events:true});
+Gallery.ContentView = function (gallery) {
+    this.content = new Slider({div: "content",
+        mode: Slider.SCROLL_HORIZONTAL,
+        disable_events: true});
     this.controller = gallery;
     this.cur_gallery = null;
     this.cur_element = 0;
@@ -217,117 +217,105 @@ Gallery.ContentView = function(gallery){
     this.arrow_right = document.getElementById("main_arrow_right");
     this.load_view = document.getElementById("loading_dialog");
     var that = this;
-    this.arrow_left.addEventListener("click",function(e){
+    this.arrow_left.addEventListener("click", function (e) {
             that.prevElement();
-            },false);
+        }, false);
 
-    this.arrow_right.addEventListener("click",function(e){
-            that.nextElement(); 
-            },false);
-}
+    this.arrow_right.addEventListener("click", function (e) {
+            that.nextElement();
+        }, false);
+};
 
-Gallery.ContentView.prototype.nextElement = function(){
-    if (this.cur_element < this.cur_gallery.getItemCount()){
+Gallery.ContentView.prototype.nextElement = function () {
+    if (this.cur_element < this.cur_gallery.getItemCount()) {
         this.cur_element++;
         this._update();
     }
-}
+};
 
-Gallery.ContentView.prototype.prevElement = function(){
-    if (this.cur_element != 0){
+Gallery.ContentView.prototype.prevElement = function () {
+    if (this.cur_element !== 0) {
         this.cur_element--;
         this._update();
     }
-}
+};
 
-Gallery.ContentView.prototype._update = function(){
-    if (this.cur_element == 0){
+Gallery.ContentView.prototype._update = function () {
+    if (this.cur_element === 0) {
         this.arrow_left.style.display = "none";
     } else {
         this.arrow_left.style.display = "block";
     }
-    if (this.cur_element == (this.cur_gallery.getItemCount() -1 )){
+    if (this.cur_element === (this.cur_gallery.getItemCount() - 1)) {
         this.arrow_right.style.display = "none";
     } else {
         this.arrow_right.style.display = "block";
     }
-    var step = this.content.getView().parentNode.offsetWidth * this.cur_element;    
-    this.content.setScrollPoint(new Point2D(step,0));
-}
+    var step = this.content.getView().parentNode.offsetWidth *
+        this.cur_element;
+    this.content.setScrollPoint(new Point2D(step, 0));
+};
 
-Gallery.ContentView.prototype.displayGallery = function(model){
+Gallery.ContentView.prototype.displayGallery = function (model) {
     this.cur_gallery = model;
     this.content.clear();
     this.content.getView().style.visibility = "hidden";
     this.loaded_elements = 0;
     this.errored_elements = 0;
     this.load_view.style.visibility = "visible";
-    var view = this.content.getView();
-    var i;
-    var total = this.cur_gallery.getItemCount();
-    var element;    
-    for ( i = 0; i < total; ++i){
+    var view = this.content.getView(), i,
+        total = this.cur_gallery.getItemCount(),
+        element, type, div;
+    for (i = 0; i < total; ++i) {
         element = this.cur_gallery.getItem(i);
-        var type = element.getItemType();
-        if (Gallery.ContentHandlers[type] !== undefined){
-            var div = document.createElement("div");
-            div.style.width="90em";
-            div.style.height="100%";
+        type = element.getItemType();
+        if (Gallery.ContentHandlers[type] !== undefined) {
+            div = document.createElement("div");
+            div.style.width = "90em";
+            div.style.height = "100%";
             div.style.display = "inline-block";
-            Gallery.ContentHandlers[type](element,div,this);
+            Gallery.ContentHandlers[type](element, div, this);
             view.appendChild(div);
-        }else{
-            alert("NO HANDLER FOR "+type);
+        } else {
+            alert("NO HANDLER FOR " + type);
         }
     }
     this.cur_element = 0;
     this._update();
-}
+};
 
-Gallery.ContentView.prototype.elementLoaded = function(){
+Gallery.ContentView.prototype.elementLoaded = function () {
     this.loaded_elements++;
     this._showContent();
-}
+};
 
-Gallery.ContentView.prototype.elementError = function(){
+Gallery.ContentView.prototype.elementError = function () {
     this.errored_elements++;
     this._showContent();
-}
+};
 
-Gallery.ContentView.prototype._showContent = function(){
-    if (this.loaded_elements + this.errored_elements >= 
-        this.cur_gallery.getItemCount()){
+Gallery.ContentView.prototype._showContent = function () {
+    if (this.loaded_elements + this.errored_elements >=
+        this.cur_gallery.getItemCount()) {
         this.content.getView().style.visibility = "visible";
         this.load_view.style.visibility = "hidden";
     }
-}
+};
 
 // ### Content Handlers ############################################
-Gallery.ContentHandlers["img"] = function(element,parent_div,callbacks){
-    img = new Image();
-    img.addEventListener("load",function(){callbacks.elementLoaded();},false);
-    img.addEventListener("error",function(){callbacks.elementError();},false);
+Gallery.ContentHandlers["img"] = function (element, parent_div, callbacks) {
+    var img = new Image();
+    img.addEventListener("load", function () {callbacks.elementLoaded(); },
+        false);
+    img.addEventListener("error", function () {callbacks.elementError(); },
+        false);
     img.src = element.getUrl();
-    parent_div.style.backgroundImage = 'url("'+element.getUrl()+'")';
+    parent_div.style.backgroundImage = 'url("' + element.getUrl() + '")';
     parent_div.style.backgroundSize = 'contain';
     parent_div.style.backgroundPosition = 'center';
     parent_div.style.backgroundRepeat = 'no-repeat';
-}
+};
 
-Gallery.ContentHandlers["vid"] = function(element, parent_div,callbacks){
-    var video = document.createElement("video");
-    video.id="vid:"+element.getUrl();
-    video.src = element.getUrl();
-    video.setAttribute("preload","metadata");
-    video.classList.add("video-js");
-    video.classList.add("vjs-default-skin");
-    video.style.width ="100%";
-    video.style.height ="100%";
-    parent_div.appendChild(video);
-    setTimeout(function(){
-            _V_(video.id,{"controls": true, "poster":element.getThumbUrl(),"preload":"metadata", "autoplay": false},
-                function(){
-                });
-            },1000);
-}
+Gallery.ContentHandlers["vid"] = function (element, parent_div, callbacks) {
 
+};
---
+++ core/video.js
@@ -0,0 +1,396 @@
+'use strict';
+
+/**
+    Create a Video Player
+    @params args Associative arary with options:
+        div: Container where to load the video player [REQUIRED]
+        
+*/
+function VideoPlayer(options) {
+    var that = this;
+    this.video = document.createElement("video");
+    this.video.addEventListener("mouseup", function () {
+        if (that.isPlaying()) {
+            that.pause();
+        } else {
+            that.play();
+        }
+    }, true);
+    this.video.preload = "metadata";
+    this.state = VideoPlayer.NO_SOURCE;
+    this.callbacks = {
+        "ready" : options.fn_ready || null,
+        "error" : options.fn_error || null,
+        "ended" : options.fn_ended || null,
+    };
+
+    //this.callbacks.ready=function (){alert("vid:ready");};
+    //this.callbacks.error=function (){alert("vid:error");};
+   // this.callbacks.ended=function (){alert("vid:ended");};
+    this.EvtStateChange = new Event();
+    // register video events
+    function onPause(e) {
+        that.pause();
+    }
+    function onLoadedMetadata(e) {
+        //that.video.removeEventListener('loadedmetadata', onLoadedMetadata);
+        that.state = VideoPlayer.READY | VideoPlayer.PAUSED;
+        that.EvtStateChange.trigger(that.state);
+        that.view.setProgress(0);
+        if (that.callbacks.ready !== null) {
+            that.callbacks.ready();
+        }
+    }
+    function onVideoEnd(e) {
+        that.state |= VideoPlayer.ENDED;
+        that.pause();
+        if (that.callbacks.ended !== null) {
+            that.callbacks.ended();
+        }
+        that.view.setProgress(0);
+        that.video.currentTime = 0;
+        // that.atTheEnd = true;
+        // if(typeof(that.view.videoEnded) === 'function')
+        //   that.view.videoEnded();
+    }
+    function onTimeUpdate(e) {
+        var progress = parseInt(that.video.currentTime /
+            that.video.duration * 100, 10);
+        that.view.setProgress(progress, that.video.currentTime);
+    }
+    function onBuffering(e) {
+        if (that.video.buffered.length !== 0) {
+            if (that.video.currentTime >= that.video.buffered.end(0)) {
+                that.state |= VideoPlayer.BUFFERING;
+            } else {
+                that.state &= ~VideoPlayer.BUFFERING;
+
+            }
+            that.EvtStateChange.trigger(that.state);
+        }
+    }
+    this.video.addEventListener('loadedmetadata', onLoadedMetadata, false);
+    this.video.addEventListener('ended', onVideoEnd, false);
+    this.video.addEventListener('timeupdate', onTimeUpdate, false);
+    this.video.addEventListener('progress', onBuffering, false);
+    this.video.addEventListener('pause', onPause, false);
+    this.view = new VideoPlayer.View(this, options);
+
+}
+/**
+    Load a video
+    @param url Video URL
+    @param poster_url Video poster URL, use "" for none.
+*/
+VideoPlayer.prototype.setSource = function (url, poster_url) {
+    this.state = VideoPlayer.LOADING;
+    this.EvtStateChange.trigger(this.state);
+    this.video.src = url;
+    this.video.poster = poster_url;
+    this.video.load();
+};
+/**
+    Get the current time in the video in seconds.
+*/
+VideoPlayer.prototype.getElapsedTime = function () {
+    if (this.state & VideoPlayer.READY) {
+        return this.video.currentTime;
+    }
+};
+/**
+    Set the current time in the video.
+    @parm time Time in seconds.
+*/
+VideoPlayer.prototype.setElapsedTime = function (time) {
+    if (this.state & VideoPlayer.READY) {
+        return this.video.currentTime = time;
+    }
+
+};
+/**
+    Set the current time in video.
+    @param percentage Percentage of completion [0 - 1]
+*/
+VideoPlayer.prototype.setElapsedPercentage = function (percentage) {
+    if (this.state & VideoPlayer.READY) {
+        this.video.currentTime = this.video.duration * percentage;
+    }
+};
+/**
+    Get the duration of the video.
+*/
+VideoPlayer.prototype.getDuration = function () {
+    if (this.state & VideoPlayer.READY) {
+        return this.video.duration;
+    }
+};
+/**
+    Pause the video.
+*/
+VideoPlayer.prototype.pause = function () {
+    if (this.state & (VideoPlayer.PLAYING | VideoPlayer.READY)) {
+        this.video.pause();
+        this.state = (this.state & ~VideoPlayer.PLAYING) | VideoPlayer.PAUSED;
+        this.EvtStateChange.trigger(this.state);
+    }
+};
+
+/**
+    Play the video.
+*/
+VideoPlayer.prototype.play = function () {
+    if (this.state & (VideoPlayer.PAUSED | VideoPlayer.READY)) {
+        this.video.play();
+        this.state = (this.state &
+            ~(VideoPlayer.PAUSED | VideoPlayer.BUFFERING | VideoPlayer.ENDED))
+            |VideoPlayer.PLAYING;
+        this.EvtStateChange.trigger(this.state);
+    }
+};
+/**
+    Check whether the video is playing.
+*/
+VideoPlayer.prototype.isPlaying = function () {
+    return this.state & VideoPlayer.PLAYING;
+};
+/**
+    Get the video state flag.
+*/
+VideoPlayer.prototype.getState = function () {
+    return this.state;
+};
+
+/** VideoPlayer States */
+VideoPlayer.NO_SOURCE = 0x00;
+VideoPlayer.LOADING = 0x01;
+VideoPlayer.READY = 0x02;
+VideoPlayer.ERROR = 0x04;
+VideoPlayer.PLAYING = 0x08;
+VideoPlayer.PAUSED = 0x10;
+VideoPlayer.SEEKING = 0x20;
+VideoPlayer.BUFFERING = 0x40;
+VideoPlayer.ENDED = 0x80;
+
+// ### VIEW ###################################
+VideoPlayer.View = function (controller, options) {
+    this.controller = controller;
+    this.container = document.getElementById(options.div);
+    this.container.classList.add("c3_video_container");
+    this.ui_elements = 0;
+    this._setupInterface();
+    this.controller.EvtStateChange.addListener(this, this._handleStateChange);
+};
+
+VideoPlayer.View.prototype._setupInterface = function () {
+    var that = this;
+    this.controls = document.createElement("div");
+    this.controls.classList.add("c3_video_controls");
+    this.bt_play = new VideoPlayer.View.BarButton({
+            "style": "c3_video_play",
+            "mouseup": function () {
+                if (that.controller.isPlaying()) {
+                    that.controller.pause();
+                } else {
+                    that.controller.play();
+                }
+            }
+        });
+    this.bt_progress = new VideoPlayer.View.Progress({
+            "style": "c3_video_progress",
+            "mouseup": function (progress) {
+                that.controller.setElapsedPercentage(progress);
+            }
+        });
+    this.bt_main = new VideoPlayer.View.MainButton(
+        {
+        "mouseup": function () {
+            if (that.controller.isPlaying()) {
+                that.controller.pause();
+            } else {
+                that.controller.play();
+            }
+        }
+    });
+    this.bt_duration = new VideoPlayer.View.Label({"text": "00:00:00"});
+    this.bt_play.hide();
+    this.container.appendChild(this.controller.video);
+    this.controls.appendChild(this.bt_play.div);
+    this.controls.appendChild(this.bt_progress.div);
+    this.controls.appendChild(this.bt_duration.div);
+    this.container.appendChild(this.controls);
+    this.container.appendChild(this.bt_main.div);
+};
+
+VideoPlayer.View.prototype._handleStateChange = function (state) {
+    if (state & VideoPlayer.LOADING) {
+        this.bt_progress.setProgress(0);
+        this.bt_main.setLoadState();
+        this.bt_play.setDisabled(true);
+        this.bt_main.show();
+        return;
+    }
+
+    if (state & VideoPlayer.READY) {
+        this.bt_play.setDisabled(false);
+        this.bt_main.setPlayState();
+    } else if (state & VideoPlayer.ERROR) {
+        this.bt_play.setDisabled(true);
+        this.bt_main.show();
+        //TODO: show error diag
+    }
+
+    if (state & VideoPlayer.PLAYING) {
+        this.bt_main.hide();
+    }
+    if (state & VideoPlayer.PAUSED && this.controller.getElapsedTime() !== 0) {
+        if (state & VideoPlayer.BUFFERING) {
+            this.bt_main.setLoadState();
+        } else {
+            this.bt_main.setPlayState();
+        }
+        this.bt_main.show();
+    }
+
+    if (state & VideoPlayer.ENDED) {
+        this.bt_main.setPlayState();
+        this.bt_main.show();
+    }
+};
+
+VideoPlayer.View.prototype.setProgress = function (progress, time) {
+    this.bt_progress.setProgress(progress);
+    if (time === undefined) {
+        this.bt_duration.setText("00:00:00");
+    } else {
+        var seconds, minutes, hours;
+        seconds = Math.floor(time % 60);
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        minutes = Math.floor(time / 60);
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        hours = Math.floor(time / 60);
+
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        this.bt_duration.setText(hours + ":" + minutes + ":" + seconds);
+    }
+};
+
+VideoPlayer.View.UIElement = function (args) {
+    this.div = document.createElement("div");
+    if (args.style !== undefined) {
+        this.div.classList.add(args.style);
+    }
+    this.disabled = false;
+};
+
+VideoPlayer.View.UIElement.prototype.isDisabled = function () {
+    return this.disabled;
+};
+VideoPlayer.View.UIElement.prototype.setDisabled = function (state) {
+    this.disabled = state;
+};
+
+VideoPlayer.View.UIElement.prototype.appendToDiv = function (dom_div) {
+    dom_div.appendChild(this.div);
+};
+
+
+VideoPlayer.View.UIElement.prototype.hide = function () {
+    this.div.style.MozTransition = "opacity 0.5s linear 0s, \
+        visibility 0s linear 0.5s";
+    this.div.style.opacity = 0;
+    this.div.style.visibility = "hidden";
+};
+
+VideoPlayer.View.UIElement.prototype.show = function () {
+    this.div.style.MozTransition = "opacity 0.5s linear 0s, \
+        visibility 0s linear 0s";
+    this.div.style.opacity = 1;
+    this.div.style.visibility = "visible";
+};
+
+VideoPlayer.View.Button = function (args) {
+    VideoPlayer.View.Button.super.constructor.call(this, args);
+    this.div.classList.add("c3_video_icon_bg");
+    if (args.mouseup !== undefined) {
+        (function (disabled, callback, bt) {
+            bt.onmouseup = function () {
+                if (!disabled) {
+                    callback();
+                }
+            };
+        } (this.disabled, args.mouseup, this.div));
+    }
+};
+
+OOP.extend(VideoPlayer.View.Button, VideoPlayer.View.UIElement);
+
+VideoPlayer.View.BarButton = function (args) {
+    VideoPlayer.View.BarButton.super.constructor.call(this, args);
+    this.div.classList.add("c3_video_button");
+};
+
+OOP.extend(VideoPlayer.View.BarButton, VideoPlayer.View.Button);
+
+VideoPlayer.View.Progress = function (args) {
+    VideoPlayer.View.Progress.super.constructor.call(this, args);
+    this.inner = document.createElement("div");
+    this.div.appendChild(this.inner);
+    var that = this;
+    this.div.onmouseup = function (e) {
+        var delta = e.clientX - that.div.offsetLeft,
+        progress = (delta / this.offsetWidth);
+        if (args.mouseup !== undefined) {
+            args.mouseup(progress);
+        }
+    };
+};
+
+VideoPlayer.View.Progress.prototype.setProgress = function (percentage) {
+    this.inner.style.width = percentage + "%";
+};
+
+OOP.extend(VideoPlayer.View.Progress, VideoPlayer.View.UIElement);
+
+VideoPlayer.View.MainButton = function (args) {
+    VideoPlayer.View.MainButton.super.constructor.call(this, args);
+    this.div.classList.add("c3_video_main_icon");
+    this.div.classList.add("c3_video_icon_main_play");
+};
+
+VideoPlayer.View.MainButton.prototype.setPlayState = function () {
+    this.div.classList.remove("c3_video_icon_main_load");
+    this.div.classList.add("c3_video_icon_main_play");
+    this.setDisabled(false);
+};
+
+VideoPlayer.View.MainButton.prototype.setLoadState = function () {
+    this.div.classList.add("c3_video_icon_main_load");
+    this.div.classList.remove("c3_video_icon_main_play");
+    this.setDisabled(true);
+};
+
+OOP.extend(VideoPlayer.View.MainButton, VideoPlayer.View.Button);
+
+
+
+VideoPlayer.View.Label = function (args) {
+    VideoPlayer.View.Label.super.constructor.call(this, args);
+    this.div.classList.add("c3_video_duration");
+    this.text = document.createElement("h1");
+    this.div.appendChild(this.text);
+    this.text.innerHTML = args.text || "";
+};
+
+VideoPlayer.View.Label.prototype.setText = function (text) {
+    this.text.innerHTML = text;
+};
+
+OOP.extend(VideoPlayer.View.Label, VideoPlayer.View.UIElement);
+
+
