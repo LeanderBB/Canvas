068191e8872ef4107ce89a7581342eb1e6cdaa2f
Leander Bessa Beernaert <leanderbb@gmail.com>
2012-11-14 10:25:24 +0000

    Added GPL license.

--- core/canvas_app.js
+++ core/canvas_app.js
@@ -1,3 +1,24 @@
+/*
+ *  This file is part of Canvas.
+ * 
+ *  Copyright (c) 2012 José Durães
+ *  Copyright (c) 2012 Leander Beernaert
+ *  Copyright (c) 2012 Tiago Oliveira
+ * 
+ *  Canvas is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Canvas is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with Zeta.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 "use strict";
 
 /**
